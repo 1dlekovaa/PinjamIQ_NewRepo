94b03cfc7678c6ba60539b512b650080568a9032
ZnKovalskia <onigen.demon@gmail.com>
2025-05-03 22:15:21 +0700

    Revisi Razaan

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,22 +4,17 @@
   data-scroll="false"
 >
   <div class="container-fluid py-1 px-3">
-    <nav aria-label="breadcrumb">
-      <ol
-        class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5"
-      >
-        <li class="breadcrumb-item text-sm">
-          <a class="opacity-5 text-white" href="javascript:;">Pages</a>
-        </li>
-        <li
-          class="breadcrumb-item text-sm text-white active"
-          aria-current="page"
-        >
-          Dashboard
-        </li>
-      </ol>
-      <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
-    </nav>
+  <nav aria-label="breadcrumb">
+  <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+    <li class="breadcrumb-item text-sm">
+      <a class="opacity-5 text-white" href="javascript:;">Pages</a>
+    </li>
+    <li class="breadcrumb-item text-sm text-white active" aria-current="page">
+      {{ $pageTitle ?? (request()->routeIs('students.*') ? 'Students' : 'Dashboard') }}
+    </li>
+  </ol>
+</nav>
+
     <div
       class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4"
       id="navbar"
